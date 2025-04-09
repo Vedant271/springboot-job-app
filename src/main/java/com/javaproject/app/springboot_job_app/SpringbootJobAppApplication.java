9c0dc9aec41a9f8e8e023407caff7fd6e1804f45
Vedant Karale <vedantkarale271@gmail.com>
2025-04-09 20:11:21 +0530

    springboot-job-app base

---
+++ src/main/java/com/javaproject/app/springboot_job_app/SpringbootJobAppApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.app.springboot_job_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJobAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/app/springboot_job_app/SpringbootJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.app.springboot_job_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
